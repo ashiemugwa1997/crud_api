0901facdbdcef04b992255942f6a535b34273a48
ashley zarter <ashleyzarter@gmail.com>
2022-03-14 19:34:18 +0200

    models

---
+++ crud_api2/routes/api.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('articles', function() {
+    // If the Content-Type and Accept headers are set to 'application/json',
+    // this will return a JSON structure. This will be cleaned up later.
+    return Article::all();
+});
+
+Route::get('uZer/{id}', function($id) {
+    return uZer::find($id);
+});
+
+Route::post('uZers', function(Request $request) {
+    return uZer::create($request->all);
+});
+
+Route::put('uZers/{id}', function(Request $request, $id) {
+    $article = Article::findOrFail($id);
+    $article->update($request->all());
+
+    return $article;
+});
+
+Route::delete('uZers/{id}', function($id) {
+    Article::find($id)->delete();
+
+    return 204;
+});
